cb16c44a97185184b9ab4c621f01953f8a9c2fad
Aman Singh <aaryanamanaa19@gmail.com>
2025-07-26 02:08:26 +0530

    hidden poweredby using helmet

--- myApp.js
+++ myApp.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
-app.use(helmet());
+
 app.use(express.static('public'));
-app.disable('strict-transport-security');
+
+// app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
