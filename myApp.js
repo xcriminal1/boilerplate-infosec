fe639560312f6e8a953255cdcb1db65f710fd73d
Aman Singh <aaryanamanaa19@gmail.com>
2025-07-29 15:27:44 +0530

    Initialize project with package.json and server.js files; set up Express server and bcrypt dependency.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const app = express();
 app.use(helmet());
 app.use(helmet.noSniff());
@@ -35,10 +35,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/hash/:input", async (req, res) => {
-  const hashed = await bcrypt.hash(req.params.input, 12);
-  res.send(`Hashed value: ${hashed}`);
-});
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 
 var express = require("express");
 var app = express();
+let bcrypt = require('bcrypt');
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
